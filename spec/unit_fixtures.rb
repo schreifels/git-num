b0db4c3d36058ad9d604148b6290f701a2edec16
Mike Schreifels <mike@schreifels.org>
2015-10-24 21:57:00 -0700

    Support filenames with special characters

--- spec/unit_fixtures.rb
+++ spec/unit_fixtures.rb
@@ -148,5 +148,65 @@ GIT_NUM_FIXTURES = {
       '',
       "\tmodified:   [2] file2"
     ].join("\n")
+  },
+
+  ##############################################################################
+
+  special_characters: {
+    porcelain: [
+      'XX "file with \"double\" quotes" -> "file still with \"double\" quotes"',
+      'XX "file with \'single\' quotes" -> "file still with \'single\' quotes"',
+      'XX "file with spaces" -> "file still with spaces"',
+      'XX file_with_!@#$%_special_chars',
+      'XX file_with_underscores'
+    ].join("\n"),
+
+    status: [
+      'On branch master',
+      'Changes to be committed:',
+      '  (use "git reset HEAD <file>..." to unstage)',
+      '',
+      "\t" + 'renamed:    "file with \\"double\\" quotes" -> "file still with \\"double\\" quotes"',
+      "\t" + 'renamed:    file with \'single\' quotes -> file still with \'single\' quotes',
+      "\t" + 'renamed:    file with spaces -> file still with spaces',
+      '',
+      'Changes not staged for commit:',
+      '  (use "git add <file>..." to update what will be committed)',
+      '  (use "git checkout -- <file>..." to discard changes in working directory)',
+      '',
+      "\t" + 'modified:   "file still with \\"double\\" quotes"',
+      "\t" + 'modified:   file still with \'single\' quotes',
+      "\t" + 'modified:   file still with spaces',
+      '',
+      'Untracked files:',
+      '  (use "git add <file>..." to include in what will be committed)',
+      '',
+      "\t" + 'file_with_!@#$%_special_chars',
+      "\t" + 'file_with_underscores'
+    ].join("\n"),
+
+    annotated_status: [
+      'On branch master',
+      'Changes to be committed:',
+      '  (use "git reset HEAD <file>..." to unstage)',
+      '',
+      "\t" + 'renamed:    [1] "file with \\"double\\" quotes" -> [2] "file still with \\"double\\" quotes"',
+      "\t" + 'renamed:    [3] file with \'single\' quotes -> [4] file still with \'single\' quotes',
+      "\t" + 'renamed:    [5] file with spaces -> [6] file still with spaces',
+      '',
+      'Changes not staged for commit:',
+      '  (use "git add <file>..." to update what will be committed)',
+      '  (use "git checkout -- <file>..." to discard changes in working directory)',
+      '',
+      "\t" + 'modified:   [2] "file still with \\"double\\" quotes"',
+      "\t" + 'modified:   [4] file still with \'single\' quotes',
+      "\t" + 'modified:   [6] file still with spaces',
+      '',
+      'Untracked files:',
+      '  (use "git add <file>..." to include in what will be committed)',
+      '',
+      "\t" + '[7] file_with_!@#$%_special_chars',
+      "\t" + '[8] file_with_underscores'
+    ].join("\n")
   }
 }
