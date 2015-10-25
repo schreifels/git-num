da647d094500c858f4bf39e962b230872690c44d
Mike Schreifels <mike@schreifels.org>
2015-10-24 18:32:12 -0700

    Extract fixtures for test readability

--- spec/unit.rb
+++ spec/unit.rb
@@ -1,71 +1,19 @@
 load 'git-num'
+require 'unit_fixtures'
 
 describe GitNum do
-  let(:git_status_porcelain) do
-    [
-      'XX file1',
-      'XX file2',
-      'XX file3'
-    ].join("\n")
-  end
-
-  before(:each) do
-    allow(GitNum).to receive(:git_status_porcelain).and_return(git_status_porcelain)
-  end
-
-  def parse_args(args)
+  def parse_args(args='')
     GitNum.parse_args(args.split(' ').map(&:strip))
   end
 
+  before(:each) do
+    allow(GitNum).to receive(:git_status_porcelain).and_return(GitNumFixtures::BASIC_PORCELAIN)
+  end
+
   describe 'status' do
-    let(:git_status) do
-      [
-        'On branch master',
-        'Changes to be committed:',
-        '  (use "git reset HEAD <file>..." to unstage)',
-        '',
-        "\tmodified:   file1",
-        '',
-        'Changes not staged for commit:',
-        '  (use "git add <file>..." to update what will be committed)',
-        '  (use "git checkout -- <file>..." to discard changes in working directory)',
-        '',
-        "\tmodified:   file2",
-        '',
-        'Untracked files:',
-        '  (use "git add <file>..." to include in what will be committed)',
-        '',
-        "\tfile3"
-      ].join("\n")
-    end
-
-    let(:expected_output) do
-      [
-        'On branch master',
-        'Changes to be committed:',
-        '  (use "git reset HEAD <file>..." to unstage)',
-        '',
-        "\tmodified:   [1] file1",
-        '',
-        'Changes not staged for commit:',
-        '  (use "git add <file>..." to update what will be committed)',
-        '  (use "git checkout -- <file>..." to discard changes in working directory)',
-        '',
-        "\tmodified:   [2] file2",
-        '',
-        'Untracked files:',
-        '  (use "git add <file>..." to include in what will be committed)',
-        '',
-        "\t[3] file3"
-      ].join("\n")
-    end
-
-    before(:each) do
-      allow(GitNum).to receive(:git_status).and_return(git_status)
-    end
-
     it 'annotates `git status` with indexes' do
-      expect { parse_args('') }.to output(expected_output).to_stdout
+      allow(GitNum).to receive(:git_status).and_return(GitNumFixtures::BASIC_STATUS)
+      expect { parse_args }.to output(GitNumFixtures::BASIC_STATUS_ANNOTATED).to_stdout
     end
   end
 
